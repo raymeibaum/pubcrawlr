5273798c97f680eff5de319768fdb57aa55ab194
Ray Meibaum <rmeibaum@me.com>
2017-03-27 13:49:26 -0400

    bars show and edit GETs working

--- controllers/bars.js
+++ controllers/bars.js
@@ -5,11 +5,16 @@ const auth = require('../helpers/auth.js');
 const User = require('../models/user.js');
 const Bar = require('../models/bar.js');
 
-router.get('/new', auth.authorize, function(req, res) {
-  res.render('bars/new.hbs', {
-    title: 'Add Bar',
-    id: req.session.currentUser._id
-  });
+router.get('/new', function(req, res) {
+  if(req.isAuthenticated()) {
+    res.render('bars/new.hbs', {
+      title: 'Add Bar',
+      username: req.user.username
+    });
+  } else {
+    res.redirect('/login');
+  }
+
 });
 
 router.post('/', auth.authorize, function(req, res) {
@@ -30,30 +35,36 @@ router.post('/', auth.authorize, function(req, res) {
     });
 });
 
-router.get('/:id', auth.authorize, function(req, res) {
-  User.findById(req.params.userId)
+router.get('/:id', function(req, res) {
+  User.findOne({username: req.params.username})
     .exec(function(err, user) {
       const bar = user.favoriteBars.id(req.params.id);
       console.log(bar);
       res.render('bars/show.hbs', {
         title: bar.name,
         bar: bar,
-        userId: req.params.userId
+        username: req.params.username,
+        isAuthenticated: req.isAuthenticated(),
+        isOwner: req.user && (req.user.username === req.params.username)
       });
     });
 });
 
-router.get('/:id/edit', auth.authorize, function(req, res) {
-  User.findById(req.params.userId)
-    .exec(function(err, user) {
-      const bar = user.favoriteBars.id(req.params.id);
-      console.log(bar);
-      res.render('bars/edit.hbs', {
-        title: bar.name,
-        bar: bar,
-        userId: req.params.userId
+router.get('/:id/edit', function(req, res) {
+  if (req.user && (req.params.username === req.user.username)) {
+    User.findOne({username: req.params.username})
+      .exec(function(err, user) {
+        const bar = user.favoriteBars.id(req.params.id);
+        console.log(bar);
+        res.render('bars/edit.hbs', {
+          title: bar.name,
+          bar: bar,
+          username: req.params.username
+        });
       });
-    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.patch('/:id', auth.authorize, function(req, res) {
