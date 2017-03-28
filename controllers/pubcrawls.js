ae9974fde2815f02b834a17c48bc8c47c5d35a45
Ray Meibaum <rmeibaum@me.com>
2017-03-27 21:39:05 -0400

    WIP pubcrawls/edit/patch

--- controllers/pubcrawls.js
+++ controllers/pubcrawls.js
@@ -36,33 +36,73 @@ router.get('/:id', function(req, res) {
     });
 });
 
-router.get(':id/edit', function(req, res) {
-  
-})
+router.get('/:id/edit', function(req, res) {
+  if(req.user && (req.params.username === req.user.username)) {
+    User.findOne({username: req.params.username})
+      .exec(function(err, user) {
+        let pubcrawl = user.pubcrawls.id(req.params.id);
+        res.render('pubcrawls/edit.hbs', {
+          title: pubcrawl.name,
+          pubcrawl: pubcrawl,
+          when: pubcrawl.when.toLocaleString(),
+          date: `${pubcrawl.when.getFullYear()}-${pubcrawl.when.getMonth()}-${pubcrawl.when.getMinutes()}`,
+          bars: user.favoriteBars,
+          username: req.params.username,
+          isAuthenticated: req.isAuthenticated()
+        });
+      });
+  } else {
+    res.redirect('/login')
+  }
+});
 
 router.post('/', function(req, res) {
-    // res.json(req.body);
   if(req.user && (req.params.username === req.user.username)) {
     User.findOne({username: req.params.username})
       .exec(function(err, user) {
         let pubcrawl = new Pubcrawl()
-          pubcrawl.name = req.body.name;
-          pubcrawl.theme = req.body.theme;
-          pubcrawl.when = new Date(req.body.date + ' ' + req.body.time);
 
-          pubcrawl.startBar = user.favoriteBars.id(req.body.startBar)
-          req.body.checkedBars.forEach(function(checkedBar) {
-            pubcrawl.bars.push(user.favoriteBars.id(checkedBar));
-          });
-          pubcrawl.transportation = req.body.transportation;
-          pubcrawl.specialInstructions = req.body.specialInstructions;
+        pubcrawl.name = req.body.name;
+        pubcrawl.theme = req.body.theme;
+        pubcrawl.when = new Date(req.body.date + ' ' + req.body.time);
+        pubcrawl.startBar = user.favoriteBars.id(req.body.startBar)
+        req.body.checkedBars.forEach(function(checkedBar) {
+          pubcrawl.bars.push(user.favoriteBars.id(checkedBar));
+        });
+        pubcrawl.transportation = req.body.transportation;
+        pubcrawl.specialInstructions = req.body.specialInstructions;
+
         user.pubcrawls.push(pubcrawl);
         user.save(function(err, user) {
           if (err) { console.log(err); }
           res.redirect(`/`);
-        })
-      })
+        });
+      });
+  } else {
+    res.redirect('/login');
+  }
+});
 
+router.patch('/:id', function(req, res) {
+  if (req.user && (req.params.username === req.user.username)) {
+    User.findOne({username: req.params.username})
+      .exec(function(err, user) {
+        let pubcrawl = user.pubcrawls.id(req.params.id);
+        pubcrawl.name = req.body.name;
+        pubcrawl.theme = req.body.theme;
+        pubcrawl.when = new Date(req.body.date + ' ' + req.body.time);
+
+        pubcrawl.startBar = user.favoriteBars.id(req.body.startBar)
+        req.body.checkedBars.forEach(function(checkedBar) {
+          pubcrawl.bars.push(user.favoriteBars.id(checkedBar));
+        });
+        pubcrawl.transportation = req.body.transportation;
+        pubcrawl.specialInstructions = req.body.specialInstructions;
+          user.save();
+          res.redirect(`/users/${req.params.username}/bars/${req.params.id}`);
+      });
+  } else {
+    redirect('/login');
   }
 });
 
